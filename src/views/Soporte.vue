0e369f7dd5facdecf1d8fe350d4a93c55a71e363
CasaAndina <alexsantino10@gmail.com>
2023-09-22 17:08:42 -0300

    [Features] vista de soporte para prueba

--- src/views/Soporte.vue
+++ src/views/Soporte.vue
@@ -1,19 +1,28 @@
+<script setup lang="ts">
+    import { ref, onMounted } from  'vue';
+
+    import Header from '@/components/Header.vue';
+</script>
 <template>
     <Header/>
+		<div class="supportView">
+			<div class="card">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cumque in sunt pariatur, tempore impedit fugit quas incidunt repudiandae distinctio minus, sequi veniam non doloremque. Ut mollitia velit iusto itaque?
+			</div>
+		</div>
 </template>
+<style lang="scss">
+	.supportView{
+		width: 100%;
+		height: 100vh;
+		padding: 10px;
+		background-color: #222;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
-<script lang="ts">
-    import Header from '@/components/Header.vue';
-
-    export default{
-        name: "SoporteView",
-        components:{
-            Header
-        },
-        data(){
-            return{
-                
-            }
-        }
-    }
-</script>
+		.card{
+			color:#fff;
+		}
+	}
+</style>
